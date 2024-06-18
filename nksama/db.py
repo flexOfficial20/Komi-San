19c1499fbcfcc548caa8844118308912adb779bf
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-18 16:49:44 +0530

    Update db.py

--- nksama/db.py
+++ nksama/db.py
@@ -1,6 +1,6 @@
 import pymongo
 import os
 
-MONGO_URL = os.environ.get('MONGO_URL', ''mongodb+srv://Mongo1:586637515hshhwfftqu@cluster0.tvy79ai.mongodb.net/?retryWrites=true&w=majority)
+MONGO_URL = os.environ.get('MONGO_URL', 'mongodb+srv://Mongo1:586637515hshhwfftqu@cluster0.tvy79ai.mongodb.net/?retryWrites=true&w=majority')
 
 database = pymongo.MongoClient(MONGO_URL)['notes']['notes']
