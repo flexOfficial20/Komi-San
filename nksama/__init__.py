a7d234a72271580704bbc872ea9fbbe4b0c3495b
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-18 16:41:32 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -7,9 +7,9 @@ help_message = []
 # class bot(Client):
 #     super().__init__(
 #         'bot',
-#                      api_id=os.environ.get('API_ID'),
-#                      api_hash=os.environ['API_HASH'],
-#                      bot_token=os.environ['BOT_TOKEN'],
+#                      api_id=os.environ.get('API_ID', '20533795'),
+#                      api_hash=os.environ['API_HASH', 'f6cadf28523943f525e706e6ace8a250'],
+#                      bot_token=os.environ['BOT_TOKEN', '7116566735:AAFOSx0aTLfpWpkM73OYjCypy6tUG4s6VfM'],
 #                      plugins=dict(root=f"{__name__}/plugins"))
 
 #     def add_cmd(module, help):
@@ -17,15 +17,15 @@ help_message = []
 #         help.update({f"{module}_help": help})
 
 bot = Client('bot',
-             api_id=os.environ.get('API_ID'),
-             api_hash=os.environ['API_HASH'],
-             bot_token=os.environ['BOT_TOKEN'],
+             api_id=os.environ.get('API_ID', '20533795'),
+             api_hash=os.environ['API_HASH', 'f6cadf28523943f525e706e6ace8a250'],
+             bot_token=os.environ['BOT_TOKEN', '7116566735:AAFOSx0aTLfpWpkM73OYjCypy6tUG4s6VfM'],
              plugins=dict(root=f"{__name__}/plugins"))
 
-PYRO_SESSION = os.environ['PYRO_SESSION']
+PYRO_SESSION = os.environ['PYRO_SESSION', 'BQE5UiMAn3Lsbv-RvFd-fg5SFj_atrbjibKf5GBq_Sbbjd8MXJ7uG6ebLyubkv1JUgT6wzwxYTHRvGtsaqXQ_M7NA1KHy5FrDGEp3k33R8zD7bKlirgJ7lf6xhF9EkWJt7yBxCMiVZdRXBvGuk6KSYFVjS0yMLhrBu3ebSqLCHk079UVCsDglYRcPC8zH9kadQsGoCvgW_85_T1GWvsdmr2-VVWIaQ2JhcWqS_Iqw9uonqAAxFdW2bufPFGajeojKWVaE-R5Ss_DMVZtkp3_4kPQ9Y_ZAyVUybJJ4B2BVCu0dAm9wGGWLbpWEiOseJYEADihEfdDLS0JC-in4ydTx4x-zTdwcQAAAAF-Ni2lAA']
 
 musicbot = Client(
     PYRO_SESSION,
-    api_id=os.environ.get('API_ID'),
-    api_hash=os.environ['API_HASH'],
+    api_id=os.environ.get('API_ID', '20533795'),
+    api_hash=os.environ['API_HASH', 'f6cadf28523943f525e706e6ace8a250'],
 )
